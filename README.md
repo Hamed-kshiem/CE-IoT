33c57101bca56e109dbaddd3be446b1ae0fec7f2
Hamed Kshiem <keshim95@gmail.com>
2020-07-07 01:40:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CE
+ CE IoT Panel  "IoT architecture"
---
+++ src/test/java/iotpanel/CE/CeApplicationTests.java
@@ -0,0 +1,13 @@
+package iotpanel.CE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
